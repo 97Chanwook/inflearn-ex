80ed86e62294458031ef13e436acaa21312a045d
97Chanuk <chanuk9531@daum.net>
2024-01-15 19:51:41 +0900

    Add Spring EL
    1. Spring EL 표현식 추가
    2. gitIgnore 추가

--- src/main/java/com/wookis/ex/thymeleaf/basic/BasicController.java
+++ src/main/java/com/wookis/ex/thymeleaf/basic/BasicController.java
@@ -1,9 +1,15 @@
 package com.wookis.ex.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/thymeleaf/basic")
@@ -19,6 +25,41 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data","Hello <b>Spring!</b>");
         return "thymeleaf/basic/text-unescaped";
+    }
+
+    /**
+     * SpringEL - 변수 표현식
+     */
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> users = new ArrayList<>();
+        users.add(userA);
+        users.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA",userA);
+        map.put("userB",userB);
+
+        model.addAttribute("user",userA);
+        model.addAttribute("users",users);
+        model.addAttribute("userMap",map);
+
+        return "thymeleaf/basic/variable";
+    }
+
+    //Sample Data 를 위한 클래스 생성
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 
 }
