8a04b0ca5a72731a2c5857f40aea9a0e16d012c2
97Chanuk <chanuk9531@daum.net>
2024-01-19 23:32:26 +0900

    Add Checkbox multi [form]
    1. 다중 체크 박스 추가
    2. label th:for = #ids 관련 내용 추가

--- src/main/java/com/wookis/ex/form/web/form/FormItemController.java
+++ src/main/java/com/wookis/ex/form/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,6 +20,17 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    // 현재 컨트롤러 안에서 호출되는 메소드들에 자동으로 모델에 담긴다.
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String,String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL","서울");
+        regions.put("BUSAN","부산");
+        regions.put("JEJU","제주");
+
+        return regions;
+    }
 
     @GetMapping
     public String items (Model model) {
@@ -37,6 +50,7 @@ public class FormItemController {
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item",new Item());
+
         return "form/addForm";
     }
 
@@ -44,6 +58,7 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item,
                            RedirectAttributes redirectAttributes) {
         log.info("item.open = {}", item.getOpen());
+        log.info("item.regions = {}", item.getRegions());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId",savedItem.getId());
