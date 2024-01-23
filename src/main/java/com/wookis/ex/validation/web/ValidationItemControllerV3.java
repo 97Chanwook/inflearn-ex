58682a7f42fb6383343a504ba419b05472af0fd7
97Chanuk <chanuk9531@daum.net>
2024-01-23 20:46:39 +0900

    Add Bean Validation
    1. Bean Validation 추가
    2. Add, Edit 폼 분리

---
+++ src/main/java/com/wookis/ex/validation/web/ValidationItemControllerV3.java
@@ -0,0 +1,89 @@
+package com.wookis.ex.validation.web;
+
+import com.wookis.ex.domain.ItemSaveForm;
+import com.wookis.ex.domain.ItemUpdateForm;
+import com.wookis.ex.domain.item.Item;
+import com.wookis.ex.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/validation/v3/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV3 {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v3/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v3/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v3/addForm";
+    }
+
+
+    @PostMapping("/add")
+    public String addItem(@Validated @ModelAttribute("item") ItemSaveForm form, BindingResult bindingResult,
+                          RedirectAttributes redirectAttributes, Model model) {
+
+        if (form.getPrice() != null && form.getQuantity() != null ) {
+            int resultPrice = form.getPrice() * form.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin",new Object[]{10000, resultPrice},null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}",bindingResult);
+            return "validation/v3/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(new Item(form.getItemName(), form.getPrice(), form.getQuantity()));
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v3/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute("item") ItemUpdateForm form) {
+        itemRepository.update(itemId, new Item(form.getItemName(), form.getPrice(), form.getQuantity()));
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+}
+
