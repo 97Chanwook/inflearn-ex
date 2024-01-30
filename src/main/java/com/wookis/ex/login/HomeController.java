a260f8f68fc3b2d516bf820ccdba586b81b8c24a
97Chanuk <chanuk9531@daum.net>
2024-01-30 19:18:09 +0900

    쿠키 사용

--- src/main/java/com/wookis/ex/login/HomeController.java
+++ src/main/java/com/wookis/ex/login/HomeController.java
@@ -1,18 +1,43 @@
 package com.wookis.ex.login;
 
 
+import com.wookis.ex.login.domain.member.Member;
+import com.wookis.ex.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 @RequestMapping("/login")
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping
+    private final MemberRepository memberRepository;
+
+//    @GetMapping
     public String home() {
         return "/login/home";
+    }
+
+    @GetMapping
+    public String homeLogin(@CookieValue(name="memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "/login/home";
+        }
+
+        //로그인
+        Member loginMember = memberRepository.findById(memberId);
+
+        if (loginMember == null) {
+            return "/login/home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "/login/loginHome";
     }
 }
--- src/main/java/com/wookis/ex/login/web/login/LoginController.java
+++ src/main/java/com/wookis/ex/login/web/login/LoginController.java
@@ -2,6 +2,8 @@ package com.wookis.ex.login.web.login;
 
 import com.wookis.ex.login.domain.login.LoginService;
 import com.wookis.ex.login.domain.member.Member;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -25,7 +27,8 @@ public class LoginController {
 
     @PostMapping("/login/sign-in")
     public String login(@Validated @ModelAttribute("loginForm") LoginForm loginForm,
-                        BindingResult bindingResult) {
+                        BindingResult bindingResult,
+                        HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "/login/sign-in/loginForm";
         }
@@ -38,7 +41,22 @@ public class LoginController {
         }
 
         //로그인 성공 처리 TODO
-        return "redirect:/login";
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료 시 쿠키 삭제)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
+        return "redirect:/login";
+    }
+
+    @PostMapping("/login/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/login";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
