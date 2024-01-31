927bdec4b0cfdfb16f3b7cb7a9fa37d5e68a62fc
97Chanuk <chanuk9531@daum.net>
2024-01-31 20:20:40 +0900

    Add Log Filter

---
+++ src/main/java/com/wookis/ex/login/WebConfig.java
@@ -0,0 +1,21 @@
+package com.wookis.ex.login;
+
+import com.wookis.ex.login.web.filter.LogFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean<Filter> logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");    //모든 URL
+
+        return filterRegistrationBean;
+    }
+}
---
+++ src/main/java/com/wookis/ex/login/web/filter/LogFilter.java
@@ -0,0 +1,36 @@
+package com.wookis.ex.login.web.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String requestURI = httpRequest.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        log.info("REQUEST [{} {}]",uuid, requestURI);
+        chain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
