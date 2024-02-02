016d18846f1a716ec93158923a84ec5df5eaf7b4
찬욱 김 <rlacksdnr9471@gmail.com>
2024-02-02 13:08:12 +0900

    Add Exception Resolver

--- src/main/java/com/wookis/ex/exception/api/ApiExceptionController.java
+++ src/main/java/com/wookis/ex/exception/api/ApiExceptionController.java
@@ -1,13 +1,13 @@
 package com.wookis.ex.exception.api;
 
+import com.wookis.ex.exception.exception.BadRequestException;
 import com.wookis.ex.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -29,6 +29,21 @@ public class ApiExceptionController {
         }
 
         return new MemberDto(id,"hello spring");
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND,"error.bad",new IllegalArgumentException());
+    }
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "OK";
     }
 
     @Data
